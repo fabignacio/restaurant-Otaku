91b667266be57702d9d835bc490335bffa395508
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-01-21 22:58:55 -0300

    Agregados los datos empresariales, hay que hacer cambios en el back

--- restaurantOtaku/src/app/pages/dashboard/jefe/controlStaff/agregar/agregar.component.ts
+++ restaurantOtaku/src/app/pages/dashboard/jefe/controlStaff/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from '../../../../../shared/services/validators/validators.service';
 import { StaffService } from '../../services/staff/staff.service';
 
 @Component({
@@ -11,18 +12,31 @@ import { StaffService } from '../../services/staff/staff.service';
 export class AgregarComponent {
 
   ingresoStaff: FormGroup = this.fb.group({
-    rut: ['1111111-1', [Validators.required]],
+    rut: ['', [Validators.required]],
     nombre: ['', [Validators.required]],
     segundoNombre: ['', [Validators.required]],
     apellido: ['', [Validators.required]],
     segundoApellido: ['', [Validators.required]],
     estadoCivil: ['', Validators.required],
-    direccion: ['', [Validators.required]]
+    direccion: ['', [Validators.required]],
+    fechaNacimiento: ['', [Validators.required]],
+    telefono: ['', [Validators.required]],
+    correoPersonal: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    correoEmpresa: ['', [Validators.required, Validators.pattern(this.vs.emailPattern)]],
+    password1: ['', [Validators.required, Validators.minLength(8)]],
+    password2: ['', [Validators.required, Validators.minLength(8)]],
+    nombreBanco: ['', [Validators.required]],
+    tipoCuenta: ['', [Validators.required]],
+    numeroCuenta: ['', [Validators.required]],
+    tipoPrevision: ['', [Validators.required]],
+    sueldoBruto: ['', Validators.required],
+    sueldoLiquido: ['', Validators.required]
   });
 
 
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private vs: ValidatorsService
   ) { }
 
   ngOnInit() { }
