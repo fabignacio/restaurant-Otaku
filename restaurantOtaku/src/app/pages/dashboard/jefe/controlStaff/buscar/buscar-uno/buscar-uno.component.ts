5c4b7332cb859659a3c2a9e8e67f0d7bc871970b
Fabian Alarcon <fabian.alarcon@xintec.cl>
2023-01-28 23:24:23 -0300

    Crud terminado para la parte del control de staff

--- restaurantOtaku/src/app/pages/dashboard/jefe/controlStaff/buscar/buscar-uno/buscar-uno.component.ts
+++ restaurantOtaku/src/app/pages/dashboard/jefe/controlStaff/buscar/buscar-uno/buscar-uno.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+
+import Swal from 'sweetalert2';
+
+import { staffListado } from '../../../../../../interfaces/staff/staff.interface';
+import { ValidatorsService } from '../../../../../../shared/services/validators/validators.service';
+
+import { StaffService } from '../../../services/staff/staff.service';
 
 @Component({
   selector: 'app-buscar-uno',
@@ -7,5 +15,58 @@ import { Component } from '@angular/core';
   ]
 })
 export class BuscarUnoComponent {
+
+  listado!: staffListado;
+  personalListado: any;
+
+  buscarStaff: FormGroup = this.fb.group({
+    rut: ['', [Validators.required]]
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private staffService: StaffService,
+    private vs: ValidatorsService
+  ) { }
+
+  validarRut = (): boolean => {
+    const rut = this.buscarStaff.get('rut')?.value;
+    const valid = this.vs.validarRut(rut);
+
+    if (valid) {
+
+      return true;
+
+    } else {
+      return false;
+    }
+
+  }
+
+  buscarUno = () => {
+    const valid = this.validarRut();
+    const rut = this.buscarStaff.get('rut')?.value;
+
+    if (valid) {
+
+      this.staffService.buscarStaff(rut).subscribe((staff) => {
+        if (staff.ok) {
+          this.listado = staff;
+          this.personalListado = this.listado.staff;
+          Swal.fire({
+            icon: 'success',
+            title: 'Bien',
+            text: 'Trabajador encontrado!',
+          });
+        } else {
+          Swal.fire('Trabajador no encontrado', rut, 'error');
+        }
+      })
+
+    } else {
+      console.log('Adios');
+    }
+  }
+
 
 }
